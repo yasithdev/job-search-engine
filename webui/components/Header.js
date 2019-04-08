687aedc10bdafb1ab0cb5f06533148a8aec79f27
Yasith <yasith@cs.odu.edu>
2019-04-08 16:35:36 -0400

    make header more intuitive

--- webui/components/Header.js
+++ webui/components/Header.js
@@ -2,11 +2,8 @@ import Link from 'next/link'
 
 const Header = (props) => (
   <nav className="navbar navbar-expand navbar-dark bg-dark mb-2">
-    <a className="navbar-brand text-white">Job Search</a>
+    <Link href="/"><a className="navbar-brand">Job Search</a></Link>
     <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <Link href="/"><a className="nav-link">Search</a></Link>
-      </li>
       <li className="nav-item">
         <Link href="/about"><a className="nav-link">About</a></Link>
       </li>
